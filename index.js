b0d9dea9122f49d53509a9ae83e01d0f931e7271
ɛʟʟɨօȶ b  =^._.^= ∫ <elliot@email.gd>
2024-12-16 21:13:50 -0500

    general cleanup, removed some features like static site hosting, api streamlining, fucked around with the log event emitters for fun

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 import Fastify from 'fastify';
 import fastifyCors from '@fastify/cors';
-import fastifyRateLimit from '@fastify/rate-limit';
+
 import dotenv from 'dotenv';
 import fs from 'node:fs/promises';
 import path from 'node:path';
 import enqueueCrawl from './lib/crawl.js';
 import { push, search, getCrawlById } from './lib/database.js';
-import { createLogEmitter, removeLogEmitter, log } from './lib/log.js';
+import { createLogEmitter, removeLogEmitter, log, defaultEmitter } from './lib/log.js';
 import {
   errorHandler,
   notFoundHandler
 } from './lib/middlewares/error-handlers.js';
+
 import {
   validateEnqueue,
   validateSearch,
@@ -20,27 +21,19 @@ import {
 // Load environment variables from .env file
 dotenv.config();
 
+// Create Fastify instance and register plugins
 const fastify = Fastify();
 const PORT = process.env.PORT || 3000;
 
+await fastify.register(fastifyCors); // cross-origin resource sharing
 
-await fastify.register(fastifyCors);
-await fastify.register(fastifyRateLimit, {
-  max: 100,
-  timeWindow: '15 minutes',
-  errorResponseBuilder: () => ({
-    code: 429,
-    error: "Too Many Requests",
-    message: "You've been ratelimited, motherfucker"
-  })
-});
 
 // Serve static files with cache control
-fastify.register(import('@fastify/static'), {
-  root: path.resolve('./front-end'),
-  prefix: '/',
-  maxAge: '1d'
-});
+//fastify.register(import('@fastify/static'), {
+//  root: path.resolve('./front-end'),
+//  prefix: '/',
+//  maxAge: '1d'
+//});
 
 // Error handler middleware
 fastify.setErrorHandler(errorHandler);
@@ -59,7 +52,11 @@ fastify.post('/submit', { schema: validateSubmit }, async (request, reply) => {
     reply.status(500).send({ error: 'Error indexing document' });
   }
 });
-
+fastify.get('/', async (request, reply) => {
+    log.info('Hello world');
+    reply.send("hello world");
+ 
+});
 // Endpoint to read logs
 fastify.get('/logs', async (request, reply) => {
   try {
@@ -98,7 +95,7 @@ fastify.get('/alive', async (request, reply) => {
 fastify.get('/crawls/:crawlId', async (request, reply) => {
 
   const { crawlId } = request.params;
-  console.log(crawlId);
+
   const crawl = await getCrawlById(crawlId);
   if (!crawl) {
     reply.status(404).send({ error: 'Crawl not found' });
@@ -106,8 +103,33 @@ fastify.get('/crawls/:crawlId', async (request, reply) => {
     reply.status(200).send(crawl);
   }
 });
+
+fastify.get('/logs/stream/all', async (request, reply) => {
+
+ 
+  const logEmitter = defaultEmitter;
+
+  reply.raw.setHeader('Content-Type', 'text/event-stream');
+  reply.raw.setHeader('Cache-Control', 'no-cache');
+  reply.raw.setHeader('Connection', 'keep-alive');
+  reply.raw.flushHeaders();
+
+  const logListener = (log) => {
+    reply.raw.write(`[${log.level}] ${log.message}\n`);
+  };
+
+  logEmitter.on('log', logListener);
+
+  // Remove listener and emitter on client disconnect
+  request.raw.on('close', () => {
+    logEmitter.removeListener('log', logListener);
+    removeLogEmitter(crawlId);
+    reply.raw.end();
+  });
+
+})
 // SSE endpoint for real-time log updates for a specific crawl ID
-fastify.get('/logs/stream/:crawlId', async (request, reply) => {
+fastify.get('/logs/stream/crawl/:crawlId', async (request, reply) => {
   const { crawlId } = request.params;
   const logEmitter = createLogEmitter(crawlId);
 
